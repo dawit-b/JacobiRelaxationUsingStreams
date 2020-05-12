f3d95b8251599cd34679123f707df47ae8e8c2be
Dawit Berhane <dawit.berhane711@gmail.com>
2020-05-12 14:13:10 -0500

    added 2D array B

--- JacobiRelaxationUsingStreams/src/jacobiRelaxation/serial/JacobiRelaxationSerial.java
+++ JacobiRelaxationUsingStreams/src/jacobiRelaxation/serial/JacobiRelaxationSerial.java
@@ -3,8 +3,37 @@ package jacobiRelaxation.serial;
 public class JacobiRelaxationSerial {
 
 	public static void main(String[] args) {
+		final int n = 32;
+		int[][] A = new int[n][n];
+		int[][] B = new int[n][n];
+		// initializing array A
+		for (int i = 1; i < n; i++) {
+			for (int j = 1; j < n; j++) {
+				A[i][j] = 0;
+			}
+		}
+		for (int i = 0; i < n; i++) {
+			A[i][0] = 10;
+			A[i][n - 1] = 10;
+			A[0][i] = 10;
+			A[n - 1][i] = 10;
+		}
+		
 		
 
+	}
+	/* prints 2D array
+	 * @param int array
+	 * 
+	 */
+	public static void printArray(int[][] array) {
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array.length; j++) {
+				System.out.print(array[i][j] + "  ");
+			}
+			System.out.println();
+			
+		}
 	}
 
 }
