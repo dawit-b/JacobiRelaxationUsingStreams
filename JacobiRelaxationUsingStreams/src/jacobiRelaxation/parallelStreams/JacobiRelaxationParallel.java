b47089de43ee9f5f5c95d7e45120d94b88b4d52b
Dawit Berhane <dawit.berhane711@gmail.com>
2020-05-13 23:17:24 -0500

    added second stream for parallel

--- JacobiRelaxationUsingStreams/src/jacobiRelaxation/parallelStreams/JacobiRelaxationParallel.java
+++ JacobiRelaxationUsingStreams/src/jacobiRelaxation/parallelStreams/JacobiRelaxationParallel.java
@@ -4,32 +4,46 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.Optional;
 import java.util.OptionalDouble;
+import java.util.OptionalInt;
 import java.util.stream.IntStream;
 
 public class JacobiRelaxationParallel {
+	static final int n = 10000;
+	static float [][] A = new float [n][n];
+	static float [][] B = new float [n][n];
 	static double change = 0;
 	public static void main(String[] args) {
-		final int n = 10000;
-		float [][] A = new float [n][n];
-		//float [][] B = new float [n][n];
+		
+		
 		double tolerance = 0.1;
 		
 		// initializing array A
 		A = initalizeArray(A);
-		float[] X = flatArray(A);
-		float[] Y = flatArray(A);
-		
+		//float[] X = flatArray(A);
+		//float[] Y = flatArray(A);
+		int counter = 0;
 		Date start_time = new Date();
+
 		do {
-			
-			OptionalDouble diff = IntStream.range(n, n*n-n).parallel()
+			counter++;
+			int iter = IntStream.range(1, n-1).parallel()
 			.map(i -> {
-				Y[i] = (X[i-n] + X[i+n] + X[i+1] + X[i-1]) / 4;
+				for(int j=1; j< n-1; j++) {
+					B[i][j] = (A[i-1][j] + A[i+1][j] + A[i][j-1] + A[i][j+1]) / 4;
+				}
 				return i;
-			})
-			.mapToDouble(i -> { change = Math.abs(X[i] - Y[i]);
-						X[i] = Y[i];
-				return change;
+				
+			}).sum();
+			
+			OptionalDouble diff = IntStream.range(1, n-1).parallel()
+			.mapToDouble(i -> {
+				double maxChange = 0;
+				for( int k=1; k<n-1; k++) {
+					change = Math.abs(B[i][k] - A[i][k]);
+					A[i][k] = B[i][k];
+					if( change > maxChange) maxChange = change;
+				}
+				return maxChange;
 			})
 			.max();
 			
@@ -37,8 +51,9 @@ public class JacobiRelaxationParallel {
 		}while(change > tolerance);
 		
 		Date end_time = new Date();
-		System.out.println(change);
+		System.out.println("Number of iteration: " + counter);
 		System.out.println("Parallel Time Elapsed: " + (end_time.getTime() - start_time.getTime()));
+		
 	}
 	/*
 	 * converts a 2D array into a stream
@@ -79,6 +94,17 @@ public class JacobiRelaxationParallel {
 			}
 		}
 		return B;
+	}
+	
+	public static void printArray(float [][] array) {
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array.length; j++) {
+				System.out.print(array[i][j] + "  ");
+			}
+			System.out.println();
+			
+		}
+		System.out.println("==================================================================");
 	}
 
 }
--- JacobiRelaxationUsingStreams/src/jacobiRelaxation/serial/JacobiRelaxationSerial.java
+++ JacobiRelaxationUsingStreams/src/jacobiRelaxation/serial/JacobiRelaxationSerial.java
@@ -9,6 +9,7 @@ public class JacobiRelaxationSerial {
 		float [][] A = new float [n][n];
 		float [][] B = new float [n][n];
 		double tolerance = 0.1;
+		
 		// initializing array A
 		for (int i = 1; i < n; i++) {
 			for (int j = 1; j < n; j++) {
@@ -24,24 +25,25 @@ public class JacobiRelaxationSerial {
 		
 		boolean done = false; 
 		double change = 0;
+		int counter = 0;
 		Date start_time = new Date();
 		do {
+			counter++;
 			for(int i=1; i< n-1; i++) {
 				for(int j=1; j< n-1; j++) {
 					B[i][j] = (A[i-1][j] + A[i+1][j]+A[i][j-1]+A[i][j+1]) / 4;
 				}
 				
 			}
-			//printArray(B);
-
+			
 			done = true;
 			for(int k=1; k<n-1; k++) {
 				for(int l=1; l<n-1; l++){
 					change = Math.abs(B[k][l]-A[k][l]);
 					if(change > tolerance) done = false;
-					//A[k][l] = B[k][l];
-					//System.out.println("Change: " + change);
+					
 				}
+				
 			}
 			for (int i = 1; i < n-1; i++) {
 				for (int j = 1; j < n-1; j++) {
@@ -49,13 +51,12 @@ public class JacobiRelaxationSerial {
 				}
 			}
 			
-			//printArray(A);
 			
 			
 		}while(!done);
 		
 		Date end_time = new Date();
-		
+		System.out.println("Number of iteration: " + counter);
 		System.out.println("Sequential time elapsed: " + (end_time.getTime() - start_time.getTime()));
 
 	}
